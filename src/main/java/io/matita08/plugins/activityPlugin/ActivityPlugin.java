29e2059652fd4c73498fc26ebecb6f4caaf7fa5b
Matita008 <155455608+Matita008@users.noreply.github.com>
2025-08-04 13:35:53 +0200

    Added JDA

--- src/main/java/io/matita08/plugins/activityPlugin/ActivityPlugin.java
+++ src/main/java/io/matita08/plugins/activityPlugin/ActivityPlugin.java
@@ -1,17 +1,28 @@
 package io.matita08.plugins.activityPlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import java.util.logging.Logger;
 
 public final class ActivityPlugin extends JavaPlugin {
-   
+   public static ActivityPlugin instance;
+   public final Logger LOG = getLogger();
    @Override
    public void onEnable() {
-      // Plugin startup logic
-      
+      LOG.info("I've been enabled!");
    }
    
    @Override
    public void onDisable() {
-      // Plugin shutdown logic
+      LOG.info("I've been disabled!");
+   }
+   
+   @Override
+   public void onLoad() {
+      instance = this;
+      
+   }
+   public static Logger logger(){
+      if(instance != null) return instance.LOG;
+      throw new RuntimeException("The plugin isn't loaded yet, How?");
    }
 }
---
+++ src/main/java/io/matita08/plugins/activityPlugin/discord/Bot.java
@@ -0,0 +1,14 @@
+package io.matita08.plugins.activityPlugin.discord;
+
+import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class Bot {
+   public final JDA bot;
+   
+   public Bot(String token) {
+      JDABuilder builder = JDABuilder.create(token, GatewayIntent.getIntents(5));
+      bot = builder.build();
+      
+   }
+}
